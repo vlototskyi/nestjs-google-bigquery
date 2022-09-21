f4c0bea12e6c7f707ed9b6129e91e767d5d713dd
Vitalii Lototskyi <vitalii.lototskyi@bytebrand.net>
2022-09-21 22:17:59 +0300

    Make module global by default

--- src/bigquery.module-definition.ts
+++ src/bigquery.module-definition.ts
@@ -3,5 +3,12 @@ import { ConfigurableModuleBuilder } from '@nestjs/common';
 
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
   new ConfigurableModuleBuilder<BigQueryOptions>()
+    .setExtras<{ isGlobal?: boolean }>(
+      { isGlobal: true },
+      (definition, extras) => {
+        console.log('extras', extras);
+        return { ...definition, global: extras.isGlobal };
+      },
+    )
     .setClassMethodName('forRoot')
     .build();
